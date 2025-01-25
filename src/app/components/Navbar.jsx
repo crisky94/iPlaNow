e5e36fa78d56d98d9071b1ca1a4be0c4bd5da38b
Cris <criistiiniilla.1994@gmail.com>
2025-01-25 22:24:45 +0100

    auth con clerk

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
